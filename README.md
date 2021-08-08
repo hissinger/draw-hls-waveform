d9fcb80d40fe4f582faad6abaf22e6bbd437709f
hissinger <loje75@gmail.com>
2021-08-08 22:36:39 +0900

    handling large recording file

--- README.md
+++ README.md
@@ -4,3 +4,31 @@ This example uses
 [wavesurfer.js](https://github.com/katspaugh/wavesurfer.js) and [hls.js](https://github.com/video-dev/hls.js).
 
 ![](./screenshot.png)
+
+## 테스트
+
+3시간 짜리 음성 녹음 파일 테스트
+
+- Chrome, Safari
+- 로컬에서 파일 다운로드 (.m3u8, .ts)
+- 소요시간: 90~100초
+- Safari에서는 메모리 부족으로 페이지 re-load 발생.
+
+## 결론
+
+wavesurfer.js가 음성 데이터를 한꺼번에 넣어야 하는 구조여서 매우 큰 audio buffer를 만들어야 한다.
+ts 단위로 decoding된 데이터를 넣어주어서 waveform을 그려주는 library가 있다면 가능하겠지만, 현재로써는 대용량 음성인 경우 클라이언트에서는 쓸 수 없다고 보는게 맞음. 직접 canvas 그리는 방법도 있겠지만 그건 내 능력 밖이라서 패스.
+막상 만들기는 했는데 서버 사이드에서 하는게 좋다고 생각함.
+
+## wavesurfer.js 외의 다른 Waveform library
+
+- waveform-data.js
+- peaks.js
+
+## ETC
+
+### Convert mp4 to m3u8
+
+```
+ffmpeg -i test.m4a -profile:v baseline -strict -2 -level 3.0 -start_number 0 -hls_time 4 -hls_list_size 0 -f hls test.m3u8
+```
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-let audio = document.getElementById("audio");
-let audioUrl =
-  "http://127.0.0.1:8080/625306f8-f5b8-11eb-912e-0241da8f597e_bridge.m3u8";
-let keyUrl = "http://127.0.0.1:8080/qybczk-hls.ssl";
-
-let worker = new Worker("./worker.js");
-worker.onmessage = (event) => {
-  wavesurfer.loadBlob(event.data);
-};
+const audio = document.getElementById("audio");
+const audioUrl =
+  "http://127.0.0.1:8080/sample/625306f8-f5b8-11eb-912e-0241da8f597e_bridge.m3u8";
+const keyUrl = "http://127.0.0.1:8080/sample/qybczk-hls.ssl";
+let segments = [];
+let initialSegment;
+let startTime, endTime;
 
 // wavesurfer
-let wavesurfer = WaveSurfer.create({
+const wavesurfer = WaveSurfer.create({
   container: document.querySelector("#waveform"),
   waveColor: "#333533",
   interact: false,
   splitChannels: true,
   responsive: true,
+  barWidth: 3,
+  partialRender: true,
   plugins: [
     WaveSurfer.timeline.create({
       container: "#wave-timeline",
@@ -34,6 +34,17 @@ let wavesurfer = WaveSurfer.create({
 wavesurfer.on("error", (error) => {
   console.log("wavesurfer error:", error);
 });
+wavesurfer.on("ready", () => {
+  console.log("wavesurfer ready");
+  endTime = new Date();
+  let timeDiff = endTime - startTime; //in ms
+  // strip the ms
+  timeDiff /= 1000;
+
+  // get seconds
+  let seconds = Math.round(timeDiff);
+  console.log(`elaped time: ${seconds} seconds`);
+});
 
 // hls
 function injectDecryptionKey(playlist) {
@@ -42,10 +53,10 @@ function injectDecryptionKey(playlist) {
 class pLoader extends Hls.DefaultConfig.loader {
   constructor(config) {
     super(config);
-    let load = this.load.bind(this);
+    const load = this.load.bind(this);
     this.load = (context, config, callbacks) => {
       if (context.type == "manifest") {
-        let onSuccess = callbacks.onSuccess;
+        const onSuccess = callbacks.onSuccess;
         callbacks.onSuccess = (response, stats, context) => {
           response.data = injectDecryptionKey(response.data);
           onSuccess(response, stats, context);
@@ -56,30 +67,150 @@ class pLoader extends Hls.DefaultConfig.loader {
   }
 }
 if (Hls.isSupported()) {
-  var hls = new Hls({
+  const hls = new Hls({
+    enableWorker: true,
+    startFragPrefetch: true,
+    progressive: true,
     pLoader: pLoader,
   });
   hls.attachMedia(audio);
-
   hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+    startTime = new Date();
     hls.loadSource(audioUrl);
     console.log("Hls.Events.MEDIA_ATTACHED");
   });
   hls.on(Hls.Events.ERROR, (event, data) => {
-    console.log("Hls.Events.ERROR", event, data);
+    if (data.details != "bufferAppendError") {
+      console.log("Hls.Events.ERROR", event, data);
+    }
+  });
+  hls.on(Hls.Events.BUFFER_CREATED, (event, data) => {
+    console.log("Hls.BUFFER_CREATED: ", data.tracks.audio.buffer);
+  });
+  hls.on(Hls.Events.BUFFER_APPENDED, (event, data) => {
+    console.log("Hls.BUFFER_APPENDED: ", data);
   });
   hls.on(Hls.Events.BUFFER_APPENDING, (event, data) => {
-    let msg = {
-      type: "data",
-      buffer: data.data,
-    };
-    worker.postMessage(msg);
+    console.log(
+      `Hls.BUFFER_APPENDING: sn:${data.frag.sn} duration: ${data.frag.duration}`
+    );
+
+    if (!initialSegment) {
+      initialSegment = data.data;
+    }
+
+    if (!segments[data.frag.sn]) {
+      segments[data.frag.sn] = {
+        sn: data.frag.sn,
+        duration: data.frag.duration,
+        totalBufferLength: 0,
+        buffers: [],
+      };
+    }
+    segments[data.frag.sn].totalBufferLength += data.data.length;
+    segments[data.frag.sn].buffers.push(data.data);
+
+    // 실제 play를 하는 것이 아니기 때문에 buffer full 발생. ts 파일을 다운 받아오지 않음.
+    // audio buffer를 reload
+    audio.pause();
+    audio.currentTime = 0;
+    audio.load();
   });
   hls.on(Hls.Events.BUFFER_EOS, async (event, data) => {
     console.log("Hls.Events.BUFFER_EOS");
-    let msg = {
-      type: "end",
-    };
-    worker.postMessage(msg);
+
+    const maxDuration = 3600 * 3;
+
+    // extract arraybuffer from segments
+    let arrayBufferList = makeArrayBufferList(maxDuration);
+    console.log(`arraybuffer count:${arrayBufferList.length}`);
+
+    // decode audio
+    let audioBuffer = await decodeAudio(arrayBufferList);
+
+    // load audiobuffer and draw waveform
+    console.log("load audioBuffer");
+    wavesurfer.loadDecodedBuffer(audioBuffer);
+
+    segments = null;
+    arrayBufferList = null;
+    audioBuffer = null;
   });
+}
+
+function makeArrayBufferList(maxDuration) {
+  let duration = 0;
+  let arrayBufferList = [];
+  for (const seg of segments) {
+    duration = duration + seg.duration;
+    if (duration > maxDuration) {
+      break;
+    }
+
+    const arraybuffer = getArrayBufferFromSegment(seg);
+    arrayBufferList.push(arraybuffer);
+  }
+
+  return arrayBufferList;
+}
+
+async function decodeAudio(arraybuffers) {
+  const audioContext = new AudioContext();
+  const decodedAudioBuffers = [];
+  let audioBufferTotalLength = 0;
+
+  for (let arraybuf of arraybuffers) {
+    // decoding segment(mp4)
+    console.log(`decoding segment`);
+    const audioBuf = await audioContext.decodeAudioData(arraybuf.buffer);
+    decodedAudioBuffers.push(audioBuf);
+    audioBufferTotalLength = audioBufferTotalLength + audioBuf.length;
+  }
+
+  console.log(
+    `audioBufferTotalLength: ${audioBufferTotalLength} count:${decodedAudioBuffers.length}`
+  );
+
+  const audioBuffer = concatAudioBuffers(
+    audioContext,
+    decodedAudioBuffers,
+    audioBufferTotalLength
+  );
+
+  return audioBuffer;
+}
+
+function getArrayBufferFromSegment(segment) {
+  const arraybuffer = new Uint8Array(
+    segment.totalBufferLength + initialSegment.length
+  );
+  let offset = 0;
+
+  arraybuffer.set(new Uint8Array(initialSegment), 0);
+  offset += initialSegment.length;
+
+  for (const buf of segment.buffers) {
+    arraybuffer.set(new Uint8Array(buf), offset);
+    offset += buf.length;
+  }
+
+  return arraybuffer;
+}
+
+function concatAudioBuffers(audioContext, buffers, length) {
+  const numberOfChannels = 2;
+  const audioBuffer = audioContext.createBuffer(
+    numberOfChannels,
+    length,
+    48000
+  );
+  let offset = Array.from({ length: numberOfChannels }, () => 0);
+  buffers.forEach((buf) => {
+    for (let i = 0; i < numberOfChannels; i++) {
+      const channel = audioBuffer.getChannelData(i);
+      channel.set(buf.getChannelData(i), offset[i]);
+      offset[i] = offset[i] + buf.length;
+    }
+  });
+  return audioBuffer;
 }
